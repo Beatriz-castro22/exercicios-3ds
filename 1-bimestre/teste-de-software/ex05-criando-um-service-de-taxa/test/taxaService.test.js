03d2b0d88b276a05826e79fcb2ff65b63bc14095
Bia★ <anabiafernandes.222@gmail.com>
2026-03-30 16:11:49 +0000

    up

---
+++ 1-bimestre/teste-de-software/ex05-criando-um-service-de-taxa/test/taxaService.test.js
@@ -0,0 +1,13 @@
+const calcularTaxa = require("../services/taxaService")
+
+beforeEach(() => {
+    console.log('teste executado.')
+})
+describe("Testa TaxaService", () => {
+    test("Aplicar 5% acima de 500", () => {
+        expect(calcularTaxa(600)).toBe(630)
+    })
+    test("Não aplica taxa até 500", () => {
+        expect(calcularTaxa(300)).toBe(300)
+    })
+})
